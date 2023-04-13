58b382d317db6476825de369a8cd7ffee7bd2797
Andre113001 <1130marcusa@gmail.com>
2023-04-13 10:41:38 +0800

    first commit

---
+++ ecommerce_app/Welcome.vb
@@ -0,0 +1,9 @@
+﻿Public Class Welcome
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+End Class
